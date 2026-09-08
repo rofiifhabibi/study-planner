e1747eb0da4456cfebad78b4097180d8a3fbae3c
rofiifhabibi <muhammadrofiif38@gmail.com>
2026-09-08 07:22:54 +0700

    fix: allow social auth users to delete account without password

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -42,11 +42,27 @@ class ProfileController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
-
         $user = $request->user();
+        
+        // If the user has a social login, we might not want to force password validation
+        // since they might have a randomly generated password.
+        // We'll only require the password if they don't have a social login,
+        // or we can just bypass it if they leave it empty and have a social login.
+        
+        $hasSocial = !empty($user->google_id) || !empty($user->telegram_id);
+        
+        if (!$hasSocial) {
+            $request->validateWithBag('userDeletion', [
+                'password' => ['required', 'current_password'],
+            ]);
+        } else {
+            // For social users, we can just optionally check password if provided.
+            if ($request->filled('password')) {
+                $request->validateWithBag('userDeletion', [
+                    'password' => ['current_password'],
+                ]);
+            }
+        }
 
         Auth::logout();
 
